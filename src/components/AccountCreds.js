502a8063751178a6ea67eca2f1fc4b8e56800bf9
RaduTuodr <tudoreduard2004@gmail.com>
2023-04-05 14:24:41 +0300

    Correct placement

--- src/components/AccountCreds.js
+++ src/components/AccountCreds.js
@@ -1,35 +1,36 @@
 import * as React from 'react';
 import { Card } from '@mui/material';
-import { CardContent, Grid, TextField } from '@material-ui/core';
+import { Button, CardContent, Grid, TextField } from '@material-ui/core';
 
 export default function AccountCredentialFields() {
 
   return (
 
-      <Card>
-        <CardContent>
+    <Card style={{maxWidth:450, margin:'120px auto', padding:'20px 5px'}}>
+      <CardContent>
+        <form>
+
           <Grid container spacing={1}>
-
-            <Grid item>
-              <TextField label="Username" placeholder="Username" variant='outlined' fullWidth/>
-            </Grid>
-          
-            <Grid item>
-             
-              <TextField
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                variant='outlined'
-              />
+            <Grid item xs={12}>
+              <TextField label="Username" placeholder="Username" variant='outlined' fullWidth />
             </Grid>
 
+            <Grid item xs={12}>
+              <TextField label="Password" type="password" autoComplete="current-password" variant='outlined'fullWidth />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button type='submit' variant='contained' color='primary' fullWidth> Submit </Button>
+            </Grid>
           </Grid>
-        </CardContent>
-      </Card>
 
-          // <TextField required id="outlined-required" label="Username" defaultValue=""/>
-          // <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password"/>
-        
+        </form>
+
+      </CardContent>
+    </Card>
+
+    // <TextField required id="outlined-required" label="Username" defaultValue=""/>
+    // <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password"/>
+
   );
 }
