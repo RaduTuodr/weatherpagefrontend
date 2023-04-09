90cee465035721dc6ed8abca385bf058e682b7ed
RaduTuodr <tudoreduard2004@gmail.com>
2023-04-09 14:54:23 +0300

    Email validation

--- src/components/AccountCreds.js
+++ src/components/AccountCreds.js
@@ -5,45 +5,92 @@ import { Button, CardContent, Grid, TextField } from '@material-ui/core';
 export default function AccountCredentialFields() {
 
   const [name, setName] = React.useState("");
-  const [description, setDescription] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  
+  const [nameError, setNameError] = React.useState(false);
+  const [emailError, setEmailError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
 
   const handleClick=(e)=> {
 
     e.preventDefault()
-    const weather={name, description}
-    console.log(weather)
 
-    fetch("http://localhost:8083/weather", {
-      method:"POST",
-      headers:{
-        "Content-Type": "application/json"
-      },
-      body:JSON.stringify(weather)
-    }).then(() => {
-      console.log("New weather")
-    })
+    setNameError(false);
+    setEmailError(false);
+    setPasswordError(false);
+
+    if (name === "") {
+      setNameError(true);
+    }
+    if (email === "" ) {
+      setEmailError(true);
+    }
+    if (password === "") {
+      setPasswordError(true);
+    }
+
+    const user={name, email, password}
+
+    if (name != "" && password != "" && emailError == false) {
+      fetch("http://localhost:8083/user", {
+        method:"POST",
+        headers:{
+          "Content-Type": "application/json"
+        },
+        body:JSON.stringify(user)
+      }).then(() => {
+        console.log("New user")
+      })
+    } else {
+      console.log("Invalid credentials");
+    }
+  }
+
+  function validateEmail() {
+
+    let pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-z]{2,3}$/;
+    setEmailError(!email.match(pattern));
   }
 
   return (
 
     <Card style={{maxWidth:450, margin:'120px auto', padding:'20px 5px'}}>
       <CardContent>
-        <form>
+        <form id = "form">
 
           <Grid container spacing={1}>
+
             <Grid item xs={12}>
-              <TextField label="Username" placeholder="Username" variant='outlined' fullWidth 
+              <TextField label="Username" placeholder="Username" variant='outlined' fullWidth required
               
               value = {name}
               onChange={(e)=>setName(e.target.value)}
+
+              error={nameError}
               />
             </Grid>
 
             <Grid item xs={12}>
-              <TextField label="Password" type="password" autoComplete="current-password" variant='outlined'fullWidth
-            
-              value = {description}
-              onChange={(e)=>setDescription(e.target.value)}
+              <TextField label="Email" placeholder="Email" variant='outlined' fullWidth required
+              
+              value = {email}
+              onChange={(e)=>setEmail(e.target.value)}
+
+              onKeyUp={(e)=>validateEmail()}
+
+              error={emailError}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField id="emailTextField" label="Password" type="password" autoComplete="current-password" variant='outlined' fullWidth required
+                         helperText="Do not share your password with anyone"
+
+              value = {password}
+              onChange={(e)=>setPassword(e.target.value)}
+
+              error={passwordError}
               />
             </Grid>
 
